481eaf3244f8fddad79704fa93e81f7de1e68d13
ivwall <ivwall@gmx.com>
2022-03-20 10:31:30 -0400

    java block work

--- addr/src/main/java/com/crtp/app/App.java
+++ addr/src/main/java/com/crtp/app/App.java
@@ -1,13 +1,44 @@
 package com.crtp.app;
 
+import com.google.bitcoin.core.ECKey;
+import com.google.bitcoin.core.NetworkParameters;
+import com.google.bitcoin.core.Address;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App  {
+    public static void main( String[] args ) {
+        System.out.println( "What's up Doc?" );
+
+        // use test net by default
+        String net = "test";
+        
+        if (args.length >= 1 && (args[0].equals("test") || args[0].equals("prod"))) {
+            net = args[0];
+            System.out.println("Using " + net + " network.");
+        }
+        
+        // create a new EC Key ...
+        ECKey key = new ECKey();
+
+        // ... and look at the key pair
+        System.out.println("We created key:\n" + key);
+        
+        // either test or production net are possible
+        final NetworkParameters netParams;
+        
+        if (net.equals("prod")) {
+            netParams = NetworkParameters.prodNet();
+        } else {
+            netParams = NetworkParameters.testNet();
+        }
+        
+        // get valid Bitcoin address from public key
+        Address addressFromKey = key.toAddress(netParams);
+        
+        System.out.println("On the " + net + " network, we can use this address:\n" + addressFromKey);
+        
     }
 }
